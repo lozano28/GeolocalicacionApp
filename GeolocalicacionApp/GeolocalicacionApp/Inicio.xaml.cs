6d02970a94bfa11066a57ac72aa51dc37dd177fe
paulLozano90 <plc90@hotmail.es>
2016-02-02 13:02:01 +0100

    Geolocalizacion

---
+++ GeolocalicacionApp/GeolocalicacionApp/Inicio.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Xamarin.Forms;
+
+namespace GeolocalicacionApp
+{
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var loc = CrossGeolocator.Current;
+            var pos = await loc.GetPositionAsync(10000);
+
+            if (pos == null)
+            {
+                await DisplayAlert("Error", "No recibo la posicion", "Ok");
+            }
+
+            txtLati.Text = pos.Latitude.ToString();
+            txtLong.Text = pos.Longitude.ToString();
+        }
+
+        private void Button_Delete(object sender, EventArgs e)
+        {
+            txtLati.Text = "";
+            txtLong.Text = "";
+        }
+    }
+}
