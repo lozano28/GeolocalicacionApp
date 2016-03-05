2fd0356c7b1a8f7bac9c281686f91ce278bafa56
paulLozano90 <plc90@hotmail.es>
2016-03-05 18:53:10 +0100

    Estilos del XAML

--- GeolocalicacionApp/GeolocalicacionApp/Inicio.xaml.cs
+++ GeolocalicacionApp/GeolocalicacionApp/Inicio.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Plugin.Geolocator;
 using Xamarin.Forms;
 
@@ -13,6 +14,25 @@ namespace GeolocalicacionApp
         public Inicio()
         {
             InitializeComponent();
+
+            //lblSubTitle.Style = Device.Styles.SubtitleStyle;
+            //lblTitle.Style = Device.Styles.TitleStyle;
+            //lblBody.Style = Device.Styles.BodyStyle;
+            //lblCaption.Style = Device.Styles.CaptionStyle;
+            //lblItemDetail.Style = Device.Styles.ListItemDetailTextStyle;
+            //lblItemText.Style = Device.Styles.ListItemTextStyle;
+
+            var estiloTitulo = new Style(typeof(Label))
+            {
+                Setters =
+                {
+                    new Setter {Property = Label.TextColorProperty, Value=Color.Yellow},
+                    new Setter {Property = Label.FontSizeProperty, Value = 30},
+                    new Setter {Property = Label.XAlignProperty, Value = TextAlignment.Center}
+                }
+            };
+
+            lblTitle.Style = estiloTitulo;
         }
 
         private async void Button_OnClicked(object sender, EventArgs e)
@@ -24,7 +44,6 @@ namespace GeolocalicacionApp
             {
                 await DisplayAlert("Error", "No recibo la posicion", "Ok");
             }
-
             txtLati.Text = pos.Latitude.ToString();
             txtLong.Text = pos.Longitude.ToString();
         }
